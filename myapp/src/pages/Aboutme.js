dfb6b561dae96511f203144b2e9f6dae64762207
Randy Faison <randyfaison@gmail.com>
2021-07-13 10:29:33 -0400

    messing with page styling

--- myapp/src/pages/Aboutme.js
+++ myapp/src/pages/Aboutme.js
@@ -31,7 +31,8 @@ const Aboutme = () => {
             </Jumbotron>
 
 
-            <Container fluid className="aboutcontainer">
+            <Container fluid className="shadow mb-5 bg-dark aboutmecontainer">
+                <Container className="p-5">
                 <Row>
                     <Col>
                     <h1>About Me</h1>
@@ -43,6 +44,7 @@ const Aboutme = () => {
                         </p>
                     </Col>
                 </Row>
+                </Container>
             </Container>
 
         </>
